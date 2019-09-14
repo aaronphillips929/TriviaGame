0d5b430f31667c1d036bf9b83d107cdeb2b0740d
Aaron Phillips <aaron.phillips929@gmail.com>
2019-09-14 15:01:26 -0500

    created file structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+var counter = 30;
+var questions = ["Who is the youngest driver on the 2019 grid?", "How many WDC's has Lewis Hamilton won?", "Which year did Scuderia Ferrari begin competing in F1?", "Which driver holds the record for most wins at the Monaco Grand Prix?", "Which circuit on the 2019 calendar is the newest?", "What does a blue flag being waved at a driver mean?", "Which manufacturer is the only tire manufacturer on the 2019 grid?", "How many races are on the 2019 calendar?"];
+var answers = [["Lando Norris", "Charles Leclerc", "Max Verstappen", "George Russell"], ["7", "5", "10", "3"], ["1920", "1930", "1950, 1970"], ["Ayerton Senna", "Michael Schumacher", "Lewis Hamilton", "Niki Lauda"], ["Baku City Circuit", "Circuit of the Americas", "Yas Marina Circuit", "Bahrain International Circuit"], ["Race Stopped", "Proceed with Caution", "Final Lap", "Lapped Traffic"], ["Bridgestone", "Michelin", "Hankook", "Pirelli"], ["21", "15", "23", "17"]];
+var correctAnswers = ["C. Max Verstappen", "B. 5", "C. 1950", "A. Ayerton Senna", "A. Baku City Circuit", "D. Lapped Traffic", "D. Pirelli", "A. 21"];
+var 
+}
